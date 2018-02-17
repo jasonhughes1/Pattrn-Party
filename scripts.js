9e9398da51a7dbd6f993cf6ca4766a74822f69d4
Jason Hughes <jasonhughes088@gmail.com>
2018-02-16 18:26:04 -0700

    Flexible cards and accordion tabs up and running

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 
+$('.tab').on('click', function() {
+    $('.tab').removeClass('tab-active');
+    $(this).addClass('tab-active');
+    $('.tab span').text('+');
+    $(this).find('span').text('-');
+    $('.card').removeClass('active-card');
+    let activeCard = `#card-${$(this).attr('id')}`;
+    $(activeCard).addClass('active-card');
+});
